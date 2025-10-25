1bf710ade717501453c2819c57cd78cefa601494
bmar <mjorvina2016@gmail.com>
2025-10-25 18:24:07 +0100

    initial commit

---
+++ src/main/kotlin/com/example/FileParser/FileParserApplication.kt
@@ -0,0 +1,11 @@
+package com.example.FileParser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileParserApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileParserApplication>(*args)
+}
