2378605ea613f6b5d943ada5ec04a7b4d35f91f1
bmar <mjorvina2016@gmail.com>
2025-10-25 18:42:17 +0100

    cleanup

--- src/main/kotlin/com/example/fileParser/FileParserApplication.kt
+++ src/main/kotlin/com/example/fileParser/FileParserApplication.kt
@@ -1,4 +1,4 @@
-package com.example.FileParser
+package com.example.fileParser
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/example/fileParser/FileParserApplicationTests.kt
+++ src/test/kotlin/com/example/fileParser/FileParserApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.FileParser
+package com.example.fileParser
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
